31fcfe3c44242d0b0be71d840fe15383092e10ad
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-09-13 17:23:44 +0900

    feat: Exercise Entity 구현

---
+++ src/main/java/com/mirae/DailyBoost/exercise/domain/repository/Exercise.java
@@ -0,0 +1,40 @@
+package com.mirae.DailyBoost.exercise.domain.repository;
+
+
+import com.mirae.DailyBoost.exercise.domain.repository.enums.ExerciseStatus;
+import com.mirae.DailyBoost.exercise.domain.repository.enums.Level;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Exercise {
+
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "exercise_id")
+  private Long id;
+
+  @Column(nullable = true)
+  private String name;
+
+  @Column(nullable = true)
+  private String description;
+
+  @Column(nullable = true)
+  @Enumerated(value = EnumType.STRING)
+  private Level level;
+
+  private ExerciseStatus status;
+
+  @Column(nullable = true, name = "youtube_link")
+  private String youtubeLink;
+
+//  private User userId;
+
+
+
+
+
+}
