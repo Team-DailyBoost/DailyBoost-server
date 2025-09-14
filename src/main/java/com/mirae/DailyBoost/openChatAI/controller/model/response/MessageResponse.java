2219432cd54885e419491766acd844ab05b3681e
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-09-14 22:00:10 +0900

    feat: MessageResponse 클래스 구현

---
+++ src/main/java/com/mirae/DailyBoost/openChatAI/controller/model/response/MessageResponse.java
@@ -0,0 +1,11 @@
+package com.mirae.DailyBoost.openChatAI.controller.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Builder
+@AllArgsConstructor
+public class MessageResponse {
+  public String message;
+
+}
