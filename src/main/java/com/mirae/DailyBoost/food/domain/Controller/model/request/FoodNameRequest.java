e72dde1887299af80aac6bc99a712887301aa5c5
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-09-16 23:12:14 +0900

    feat: FoodNameRequest 요청 DTO 구현

---
+++ src/main/java/com/mirae/DailyBoost/food/domain/Controller/model/request/FoodNameRequest.java
@@ -0,0 +1,11 @@
+package com.mirae.DailyBoost.food.domain.Controller.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class FoodNameRequest {
+
+  String name;
+}
