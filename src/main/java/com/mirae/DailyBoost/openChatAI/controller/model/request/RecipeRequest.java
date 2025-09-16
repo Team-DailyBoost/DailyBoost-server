fd95a8088008534cf2c2e56814cfb482e5d0e562
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-09-16 23:15:38 +0900

    feat: RecipeRequest 요청 DTO 구현

---
+++ src/main/java/com/mirae/DailyBoost/openChatAI/controller/model/request/RecipeRequest.java
@@ -0,0 +1,11 @@
+package com.mirae.DailyBoost.openChatAI.controller.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RecipeRequest {
+
+  String userInput;
+}
