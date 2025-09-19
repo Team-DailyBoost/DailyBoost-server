808ddbce6e49a0381c336a03ccfbcd60327f4607
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-09-19 23:51:24 +0900

    feat: 사용자 정보 기입 요청 DTO 구현

---
+++ src/main/java/com/mirae/DailyBoost/user/domain/controller/model/request/UserRequest.java
@@ -0,0 +1,18 @@
+package com.mirae.DailyBoost.user.domain.controller.model.request;
+
+import com.mirae.DailyBoost.user.domain.repository.HealthInfo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequest {
+  private HealthInfo healthInfo;
+  private String goal;
+  private String allergy;
+
+}
