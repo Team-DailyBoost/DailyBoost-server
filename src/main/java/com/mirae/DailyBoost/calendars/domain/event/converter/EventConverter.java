c35c06eb4fa39cee260adf0b6ba33bc8351632f5
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-09-23 16:58:20 +0900

    feat: 일정 DTO 변환 클래스 구현

---
+++ src/main/java/com/mirae/DailyBoost/calendars/domain/event/converter/EventConverter.java
@@ -0,0 +1,55 @@
+package com.mirae.DailyBoost.calendars.domain.event.converter;
+
+import com.mirae.DailyBoost.calendars.domain.calendars.repository.Calendars;
+import com.mirae.DailyBoost.calendars.domain.event.controller.model.request.EventsRequest;
+import com.mirae.DailyBoost.calendars.domain.event.controller.model.response.EventInfo;
+import com.mirae.DailyBoost.calendars.domain.event.controller.model.response.EventResponse;
+import com.mirae.DailyBoost.calendars.domain.event.controller.model.response.EventsResponse;
+import com.mirae.DailyBoost.calendars.domain.event.repository.Event;
+import com.mirae.DailyBoost.common.annotation.Converter;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Converter
+public class EventConverter {
+
+  public Event toEntity(Calendars calendar, EventsRequest eventsRequest) {
+    return Event.builder()
+        .calendar(calendar)
+        .title(eventsRequest.getTitle())
+        .description(eventsRequest.getDescription())
+        .startTime(eventsRequest.getStartTime())
+        .endTime(eventsRequest.getEndTime())
+        .createdAt(LocalDateTime.now())
+        .build();
+
+  }
+
+  public EventsResponse toResponse(List<Event> events) {
+
+    return EventsResponse.builder()
+        .eventInfos(events.stream().map(event -> {
+          return EventInfo.builder()
+              .id(event.getId())
+              .calendarId(event.getCalendar().getId())
+              .title(event.getTitle())
+              .description(event.getDescription())
+              .startTime(event.getStartTime())
+              .endTime(event.getEndTime())
+              .build();
+        }).toList())
+        .build();
+  }
+
+  public EventResponse toResponse(Event event) {
+    return EventResponse.builder()
+        .id(event.getId())
+        .calendarId(event.getCalendar().getId())
+        .title(event.getTitle())
+        .description(event.getDescription())
+        .startTime(event.getStartTime())
+        .endTime(event.getEndTime())
+        .build();
+  }
+
+}
