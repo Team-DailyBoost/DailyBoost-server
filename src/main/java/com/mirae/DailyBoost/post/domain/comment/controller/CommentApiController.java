94a7f1661b13d95c38fe2c92252f3fb21c654634
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-10-01 19:54:29 +0900

    feat: 댓글 작성, 수정, 삭제, 조회 기능 구현

---
+++ src/main/java/com/mirae/DailyBoost/post/domain/comment/controller/CommentApiController.java
@@ -0,0 +1,62 @@
+package com.mirae.DailyBoost.post.domain.comment.controller;
+
+import com.mirae.DailyBoost.common.annotation.LoginUser;
+import com.mirae.DailyBoost.common.api.Api;
+import com.mirae.DailyBoost.common.model.MessageResponse;
+import com.mirae.DailyBoost.oauth.dto.UserDTO;
+import com.mirae.DailyBoost.post.domain.comment.business.CommentBusiness;
+import com.mirae.DailyBoost.post.domain.comment.controller.model.request.CommentRequest;
+import com.mirae.DailyBoost.post.domain.comment.controller.model.request.CommentUnregisterRequest;
+import com.mirae.DailyBoost.post.domain.comment.controller.model.request.CommentUpdateRequest;
+import com.mirae.DailyBoost.post.domain.comment.controller.model.response.CommentResponse;
+import java.util.List;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/comment")
+public class CommentApiController {
+
+  private final CommentBusiness commentBusiness;
+
+  @PostMapping("/create")
+  public Api<MessageResponse> create(@LoginUser UserDTO userDTO, @RequestBody @Valid CommentRequest commentRequest) {
+    return Api.OK(commentBusiness.create(userDTO, commentRequest));
+  }
+
+  @PostMapping("/unregister")
+  public Api<MessageResponse> unregister(@LoginUser UserDTO userDTO, @RequestBody @Valid CommentUnregisterRequest commentUnregisterRequest) {
+    return Api.OK(commentBusiness.unregister(userDTO, commentUnregisterRequest));
+  }
+
+  @PostMapping("/update")
+  public Api<MessageResponse> update(@LoginUser UserDTO userDTO, @RequestBody @Valid CommentUpdateRequest commentUpdateRequest) {
+    return Api.OK(commentBusiness.update(userDTO, commentUpdateRequest));
+  }
+
+  // 수정 할 것
+  @PostMapping("/like/{commentId}")
+  public Api<MessageResponse> like(@LoginUser UserDTO userDTO, @PathVariable Long commentId) {
+    return Api.OK(commentBusiness.like(userDTO, commentId));
+  }
+
+  // 수정 할 것
+  @PostMapping("/unLike/{commentId}")
+  public Api<MessageResponse> unLike(@LoginUser UserDTO userDTO, @PathVariable Long commentId) {
+    return Api.OK(commentBusiness.unLike(userDTO, commentId));
+  }
+
+
+  @GetMapping("/{postId}")
+  public Api<List<CommentResponse>> getCommentsByPostId(@PathVariable Long postId) {
+    return Api.OK(commentBusiness.getByPostId(postId));
+  }
+
+}
