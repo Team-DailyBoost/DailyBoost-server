31b51bd51b48ec8d3c912b31332dacabc50f1c4e
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-10-01 19:54:41 +0900

    feat: 댓글 작성, 수정, 삭제, 조회 기능 구현

---
+++ src/main/java/com/mirae/DailyBoost/post/domain/comment/business/CommentBusiness.java
@@ -0,0 +1,145 @@
+package com.mirae.DailyBoost.post.domain.comment.business;
+
+import com.mirae.DailyBoost.common.annotation.Business;
+import com.mirae.DailyBoost.common.converter.MessageConverter;
+import com.mirae.DailyBoost.common.model.MessageResponse;
+import com.mirae.DailyBoost.oauth.dto.UserDTO;
+import com.mirae.DailyBoost.post.domain.comment.controller.model.request.CommentRequest;
+import com.mirae.DailyBoost.post.domain.comment.controller.model.request.CommentUnregisterRequest;
+import com.mirae.DailyBoost.post.domain.comment.controller.model.request.CommentUpdateRequest;
+import com.mirae.DailyBoost.post.domain.comment.controller.model.response.CommentResponse;
+import com.mirae.DailyBoost.post.domain.comment.converter.CommentConverter;
+import com.mirae.DailyBoost.post.domain.comment.repository.Comment;
+import com.mirae.DailyBoost.post.domain.comment.service.CommentService;
+import com.mirae.DailyBoost.post.domain.post.repository.Post;
+import com.mirae.DailyBoost.post.domain.post.repository.enums.PostStatus;
+import com.mirae.DailyBoost.post.domain.post.service.PostService;
+import com.mirae.DailyBoost.user.domain.repository.User;
+import com.mirae.DailyBoost.user.domain.service.UserService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@Business
+@Transactional(readOnly = false)
+@RequiredArgsConstructor
+public class CommentBusiness {
+
+  private final CommentService commentService;
+  private final UserService userService;
+  private final PostService postService;
+  private final CommentConverter commentConverter;
+  private final MessageConverter messageConverter;
+
+
+  public MessageResponse create(UserDTO userDTO, CommentRequest commentRequest) {
+    User user = userService.getById(userDTO.getId())
+        .orElseThrow(() -> new IllegalArgumentException("USER_NOT_FOUND"));
+
+    Post post = postService.getByIdAndStatus(commentRequest.getPostId(), PostStatus.REGISTERED)
+        .orElseThrow(() -> new IllegalArgumentException("POST_NOT_FOUND"));
+
+    Comment comment = commentConverter.toEntity(user, post, commentRequest);
+
+    post.addComment(comment);
+
+    commentService.save(comment);
+
+    return messageConverter.toResponse("댓글이 작성되었습니다.");
+  }
+
+  public MessageResponse unregister(UserDTO userDTO, CommentUnregisterRequest commentUnregisterRequest) {
+    User user = userService.getById(userDTO.getId())
+        .orElseThrow(() -> new IllegalArgumentException("USER_NOT_FOUND"));
+
+    Comment comment = commentService.getById(commentUnregisterRequest.getCommentId())
+        .orElseThrow(() -> new IllegalArgumentException("COMMENT_NOT_FOUND"));
+
+    validateCommentOwner(comment, user);
+
+    Post post = postService.getById(commentUnregisterRequest.getPostId())
+        .orElseThrow(() -> new IllegalArgumentException("POST_NOT_FOUND"));
+
+    Boolean isExistsComment = postService.existsByComments(post.getId(), comment);
+
+    if (!isExistsComment) {
+      throw new IllegalArgumentException("게시글에 작성된 댓글이 아닙니다.");
+    }
+
+    post.removeComment(comment);
+
+    comment.unregister();
+
+    commentService.save(comment);
+
+    return messageConverter.toResponse("댓글이 삭제되었습니다.");
+  }
+
+  public MessageResponse update(UserDTO userDTO, CommentUpdateRequest commentUpdateRequest) {
+    User user = userService.getById(userDTO.getId())
+        .orElseThrow(() -> new IllegalArgumentException("USER_NOT_FOUND"));
+
+    Comment comment = commentService.getById(commentUpdateRequest.getCommentId())
+        .orElseThrow(() -> new IllegalArgumentException("COMMENT_NOT_FOUND"));
+
+    validateCommentOwner(comment, user);
+
+    Post post = postService.getById(commentUpdateRequest.getPostId())
+        .orElseThrow(() -> new IllegalArgumentException("POST_NOT_FOUND"));
+
+    Boolean isExistsComment = postService.existsByComments(post.getId(), comment);
+
+    if (!isExistsComment) {
+      throw new IllegalArgumentException("게시글에 작성된 댓글이 아닙니다.");
+    }
+
+    comment.updateInfo(commentUpdateRequest.getContent());
+
+    commentService.save(comment);
+
+    return messageConverter.toResponse("댓글이 수정되었습니다.");
+
+  }
+
+  public MessageResponse like(UserDTO userDTO, Long id) {
+
+    Comment comment = commentService.getById(id)
+        .orElseThrow(() -> new IllegalArgumentException("POST_NOT_FOUND"));
+
+    comment.like();
+    commentService.save(comment);
+
+    return messageConverter.toResponse("좋아요가 눌렸습니다."); // 띄우기 X
+  }
+
+  public MessageResponse unLike(UserDTO userDTO, Long id) {
+
+    Comment comment = commentService.getById(id)
+        .orElseThrow(() -> new IllegalArgumentException("POST_NOT_FOUND"));
+
+    comment.unLike();
+    commentService.save(comment);
+
+    return messageConverter.toResponse("싫어요가 눌렸습니다."); // 띄우기 X
+  }
+
+
+  @Transactional(readOnly = true)
+  public List<CommentResponse> getByPostId(Long postId) {
+    Post post = postService.getById(postId)
+        .orElseThrow(() -> new IllegalArgumentException("POST_NOT_FOUND"));
+
+    List<Comment> comments = post.getComments();
+
+    return commentConverter.toCommentResponse(comments);
+  }
+
+  private void validateCommentOwner(Comment comment, User user) {
+    Long commentAuthorId = comment.getUser().getId();
+    Long userId = user.getId();
+
+    if (commentAuthorId != userId) {
+      throw new IllegalArgumentException("편집 권한이 없습니다.");
+    }
+  }
+}
