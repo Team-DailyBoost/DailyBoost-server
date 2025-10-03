ef3e20a95b9f507149e745a5a14e5768717fcd2e
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-10-03 20:07:53 +0900

    feat: @Operation 추가

--- src/main/java/com/mirae/DailyBoost/user/domain/controller/UserController.java
+++ src/main/java/com/mirae/DailyBoost/user/domain/controller/UserController.java
@@ -6,9 +6,14 @@ import com.mirae.DailyBoost.common.model.MessageResponse;
 import com.mirae.DailyBoost.oauth.dto.UserDTO;
 import com.mirae.DailyBoost.user.domain.business.UserBusiness;
 import com.mirae.DailyBoost.user.domain.controller.model.request.UserRequest;
-import jakarta.servlet.http.HttpSession;
+import com.mirae.DailyBoost.user.domain.controller.model.request.UserUpdateRequest;
+import com.mirae.DailyBoost.user.domain.controller.model.request.VerifyCodeRequest;
+import com.mirae.DailyBoost.user.domain.controller.model.response.UserResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,24 +26,50 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
   private final UserBusiness userBusiness;
-  private final HttpSession session;
 
   @PostMapping("/initInfo")
+  @Operation(
+      summary = "사용자 헬스 정보 등록",
+      description = "회원가입 시 사용자 헬스 정보를 등록합니다."
+  )
   public Api<MessageResponse> userInfoInsert(@LoginUser UserDTO userDTO, @RequestBody UserRequest userRequest) {
-    log.info("현재 세션 ID: {}", session.getId());
-    log.info("세션 user 속성: {}", session.getAttribute("user"));
     return Api.OK(userBusiness.userInfoInsert(userDTO, userRequest));
   }
 
   @PostMapping("/unregister")
+  @Operation(
+      summary = "계정 등록 해제",
+      description = "로그인 한 사용자의 계정을 등록 해제합니다."
+  )
   public Api<MessageResponse> unregister(@LoginUser UserDTO userDTO) {
     return Api.OK(userBusiness.unregister(userDTO));
   }
 
-  @PostMapping("/revoke")
-  public Api<MessageResponse> revokeUnregister(@LoginUser UserDTO userDTO) {
-    return Api.OK(userBusiness.revokeUnregistration(userDTO));
+  @PostMapping("/update")
+  @Operation(
+      summary = "사용자 정보 수정",
+      description = "자신의 정보를 수정합니다."
+  )
+  public Api<MessageResponse> update(@LoginUser UserDTO userDTO, UserUpdateRequest userUpdateRequest) {
+    return Api.OK(userBusiness.updateUserInfo(userDTO, userUpdateRequest));
   }
 
+  @PostMapping("/recover")
+  @Operation(
+      summary = "계정 복구",
+      description = "사용자의 계정을 다시 복구합니다."
+  )
+  public Api<MessageResponse> recoverUserAccount(@RequestBody VerifyCodeRequest request) {
+    return Api.OK(userBusiness.recoverUserAccount(request));
+  }
+
+  @GetMapping
+  @Operation(
+      summary = "사용자 프로필 조회",
+      description = "사용자의 프로필을 조회합니다."
+  )
+  public Api<UserResponse> userProfile(@PathVariable Long userId) {
+    return Api.OK(userBusiness.getByIdAndStatus(userId));
+  }
 
 }
