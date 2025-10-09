af0cc4add55d42467363224ad86fc69573aa14b4
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-10-09 11:54:01 +0900

    feat: 유저 예외 클래스 핸들러 구현

--- src/main/java/com/mirae/DailyBoost/user/exception/UserExceptionHandler.java
+++ src/main/java/com/mirae/DailyBoost/user/exception/UserExceptionHandler.java
@@ -2,7 +2,11 @@ package com.mirae.DailyBoost.user.exception;
 
 import com.mirae.DailyBoost.global.api.Api;
 import com.mirae.DailyBoost.global.errorCode.UserErrorCode;
+import com.mirae.DailyBoost.user.exception.user.AlreadyUnregisteredException;
+import com.mirae.DailyBoost.user.exception.user.DormantAccountException;
 import com.mirae.DailyBoost.user.exception.user.UserNotFoundException;
+import com.mirae.DailyBoost.user.exception.user.VerificationCodeExpiredOrNotFoundException;
+import com.mirae.DailyBoost.user.exception.user.VerificationCodeMismatchException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,8 +19,39 @@ public class UserExceptionHandler {
 
   @ExceptionHandler(value = UserNotFoundException.class)
   public ResponseEntity<Api<Object>> userNotFoundException(UserNotFoundException e) {
+    log.error("", e);
     return ResponseEntity.status(HttpStatus.NOT_FOUND)
         .body(Api.error(UserErrorCode.USER_NOT_FOUND));
   }
+
+  @ExceptionHandler(value = AlreadyUnregisteredException.class)
+  public ResponseEntity<Api<Object>> alreadyUnregisteredException(AlreadyUnregisteredException e) {
+    log.error("", e);
+    return ResponseEntity.status(HttpStatus.CONFLICT)
+        .body(Api.error(UserErrorCode.ALREADY_UNREGISTERED));
+  }
+
+  @ExceptionHandler(value = DormantAccountException.class)
+  public ResponseEntity<Api<Object>> dormantAccountException(DormantAccountException e) {
+    log.error("", e);
+    return ResponseEntity.status(HttpStatus.LOCKED)
+        .body(Api.error(UserErrorCode.DORMANT_ACCOUNT));
+  }
+
+  @ExceptionHandler(value = VerificationCodeMismatchException.class)
+  public ResponseEntity<Api<Object>> verificationCodeMismatchException(VerificationCodeMismatchException e) {
+    log.error("", e);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(Api.error(UserErrorCode.VERIFICATION_CODE_MISMATCH));
+  }
+
+  @ExceptionHandler(value = VerificationCodeExpiredOrNotFoundException.class)
+  public ResponseEntity<Api<Object>> verificationCodeExpiredOrNotFoundException(VerificationCodeExpiredOrNotFoundException e) {
+    log.error("", e);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(Api.error(UserErrorCode.VERIFICATION_CODE_EXPIRED_OR_NOT_FOUND));
+  }
+
+
 
 }
