eb4762d7ea70429851e10f9926a5ecb4e977174c
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-10-09 10:20:39 +0900

    feat: 식단 객체 컨버터 구현

--- src/main/java/com/mirae/DailyBoost/food/domain/converter/FoodConverter.java
+++ src/main/java/com/mirae/DailyBoost/food/domain/converter/FoodConverter.java
@@ -1,37 +1,58 @@
 package com.mirae.DailyBoost.food.domain.converter;
 
 import com.mirae.DailyBoost.common.annotation.Converter;
-import com.mirae.DailyBoost.food.domain.Controller.model.response.FoodResponse;
+import com.mirae.DailyBoost.food.domain.controller.model.response.FoodResponse;
 import com.mirae.DailyBoost.food.domain.repository.Food;
-import com.mirae.DailyBoost.openChatAI.controller.model.request.FoodRecommendation;
+import com.mirae.DailyBoost.food.domain.controller.model.response.FoodRecommendation;
+import com.mirae.DailyBoost.food.domain.repository.enums.FoodStatus;
+import com.mirae.DailyBoost.user.domain.repository.User;
 import java.util.List;
 
 @Converter
 public class FoodConverter {
 
-  public List<Food> toEntity(FoodRecommendation foodRecommendation) {
-    return foodRecommendation.getFoodInfoDto().stream().map(foodInfoDto -> {
+  public List<Food> toEntity(User user, List<FoodRecommendation> foodsRecommendation) {
+    return foodsRecommendation.stream().map(foodRecommendation -> {
       return Food.builder()
-          .name(foodInfoDto.getName())
-          .calory(foodInfoDto.getCalory())
-          .carbohydrate(foodInfoDto.getCarbohydrate())
-          .protein(foodInfoDto.getProtein())
-          .fat(foodInfoDto.getFat())
-          .foodKind(foodInfoDto.getFoodKind())
-          .description(foodInfoDto.getDescription())
+          .name(foodRecommendation.getName())
+          .calory(foodRecommendation.getCalory())
+          .carbohydrate(foodRecommendation.getCarbohydrate())
+          .protein(foodRecommendation.getProtein())
+          .fat(foodRecommendation.getFat())
+          .foodKind(foodRecommendation.getFoodKind())
+          .description(foodRecommendation.getDescription())
+          .status(FoodStatus.UNREGISTERED)
+          .user(user)
           .build();
     }).toList();
   }
 
-  public FoodResponse toResponse(Food food) {
-    return FoodResponse.builder()
-        .name(food.getName())
-        .calory(food.getCalory())
-        .carbohydrate(food.getCarbohydrate())
-        .protein(food.getProtein())
-        .fat(food.getFat())
-        .foodKind(food.getFoodKind())
-        .description(food.getDescription())
+  public Food toRecipeFoodEntity(User user, FoodRecommendation foodRecommendation) {
+    return Food.builder()
+        .name(foodRecommendation.getName())
+        .calory(foodRecommendation.getCalory())
+        .carbohydrate(foodRecommendation.getCarbohydrate())
+        .protein(foodRecommendation.getProtein())
+        .fat(foodRecommendation.getFat())
+        .foodKind(foodRecommendation.getFoodKind())
+        .description(foodRecommendation.getDescription())
+        .status(FoodStatus.UNREGISTERED)
+        .user(user)
         .build();
+  }
+
+  public List<FoodResponse> toResponse(List<Food> foods) {
+    return foods.stream().map(food -> {
+      return FoodResponse.builder()
+          .id(food.getId())
+          .name(food.getName())
+          .calory(food.getCalory())
+          .carbohydrate(food.getCarbohydrate())
+          .protein(food.getProtein())
+          .fat(food.getFat())
+          .foodKind(food.getFoodKind())
+          .description(food.getDescription())
+          .build();
+    }).toList();
   }
 }
