5024a7d8d2bc6a3016f39ae6262a085b7844fab9
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-10-09 10:25:40 +0900

    feat: 식단 키워드 검색, 일일 검색, 주간 검색 기능 구현

--- src/main/java/com/mirae/DailyBoost/food/domain/service/FoodService.java
+++ src/main/java/com/mirae/DailyBoost/food/domain/service/FoodService.java
@@ -1,8 +1,14 @@
 package com.mirae.DailyBoost.food.domain.service;
 
+import static java.time.temporal.TemporalAdjusters.previousOrSame;
+
 import com.mirae.DailyBoost.food.domain.repository.Food;
 import com.mirae.DailyBoost.food.domain.repository.FoodRepository;
 import com.mirae.DailyBoost.food.domain.repository.enums.FoodKind;
+import com.mirae.DailyBoost.food.domain.repository.enums.FoodStatus;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,17 +19,38 @@ public class FoodService {
 
   private final FoodRepository foodRepository;
 
-  public Food register(Food food) {
+
+  public Food save(Food food) {
     return foodRepository.save(food);
+  }
+
+  public List<Food> saveAll(List<Food> foods) {
+    return foodRepository.saveAll(foods);
   }
 
   public Optional<Food> getByIdAndKind(Long id, FoodKind foodKind) {
     return foodRepository.findByIdAndFoodKind(id, foodKind);
   }
 
-  public Optional<Food> getByIdAndName(Long id, String name) {
-    return foodRepository.findByIdAndName(id, name);
+  public List<Food> getByKeywordAndStatus(Long userId, String keyword, FoodStatus status) {
+    return foodRepository.findByUser_idAndKeywordAndStatus(userId, keyword, status);
   }
 
 
+  public Optional<Food> getById(Long id) {
+    return foodRepository.findById(id);
+  }
+
+  public List<Food> getByStatusAndToday(Long userId, FoodStatus status, LocalDate date) {
+    return foodRepository.findByUser_idAndStatusAndRegisteredAt(userId, status, date);
+
+  }
+
+  public List<Food> getByStatusAndWeekly(Long userId, FoodStatus foodStatus) {
+    LocalDate today = LocalDate.now();
+    LocalDate start = today.with(previousOrSame(DayOfWeek.MONDAY));
+    LocalDate end   = start.plusDays(6); // 일요일
+
+    return foodRepository.findByUser_idAndStatusAndRegisteredAtBetweenOrderByRegisteredAtAsc(userId, foodStatus, today, end);
+  }
 }
