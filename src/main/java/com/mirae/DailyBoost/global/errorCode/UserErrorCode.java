fcd87a2dc3991c56e5ddf3efa80167c95db6bf7b
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-10-09 11:52:44 +0900

    feat: 유저 에러 코드 구현

--- src/main/java/com/mirae/DailyBoost/global/errorCode/UserErrorCode.java
+++ src/main/java/com/mirae/DailyBoost/global/errorCode/UserErrorCode.java
@@ -2,16 +2,18 @@ package com.mirae.DailyBoost.global.errorCode;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum UserErrorCode implements ErrorCode {
 
   USER_NOT_FOUND(404, 1100, "사용자를 찾을 수 없습니다."),
-  EXISTS_USER_EMAIL(403, 1101, "이미 존재하는 아이디입니다."),
-  EXISTS_USER_NAME(403, 1102, "이미 존재하는 이름입니다."),
-  LOGIN_FAIL(401, 1103, "로그인 정보가 일치하지 않습니다."),
-  USER_UNREGISTER_FAIL(500, 1104, "회원 탈퇴에 실패했습니다.")
+  ALREADY_UNREGISTERED(HttpStatus.CONFLICT.value(), 1101, "계정이 이미 삭제된 상태입니다. 로그인을 하려면 계정 복구를 해주세요."),
+  DORMANT_ACCOUNT(HttpStatus.LOCKED.value(), 1102, "회원님의 계정이 휴면계정입니다. 로그인을 하려면 계정 복구를 해주세요."),
+  VERIFICATION_CODE_MISMATCH(HttpStatus.BAD_REQUEST.value(), 1103, "인증 코드가 일치하지 않습니다."),
+  VERIFICATION_CODE_EXPIRED_OR_NOT_FOUND(HttpStatus.BAD_REQUEST.value(), 1104, "인증 코드가 만료되었거나 존재하지 않습니다."),
+  USER_UNREGISTER_FAIL(500, 1105, "회원 탈퇴에 실패했습니다.")
   ;
 
   private final Integer httpCode;
