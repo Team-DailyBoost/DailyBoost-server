a55d73802158d2fe50c8f9127129775351eb0495
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-10-09 10:22:45 +0900

    feat: 유저 검증과 식단 추천 추가 구현

--- src/main/java/com/mirae/DailyBoost/food/domain/controller/FoodController.java
+++ src/main/java/com/mirae/DailyBoost/food/domain/controller/FoodController.java
@@ -3,8 +3,6 @@ package com.mirae.DailyBoost.food.domain.controller;
 import com.mirae.DailyBoost.common.annotation.LoginUser;
 import com.mirae.DailyBoost.common.api.Api;
 import com.mirae.DailyBoost.common.model.MessageResponse;
-import com.mirae.DailyBoost.food.domain.controller.model.request.FoodKindRequest;
-import com.mirae.DailyBoost.food.domain.controller.model.request.FoodNameRequest;
 import com.mirae.DailyBoost.food.domain.controller.model.request.RecipeRequest;
 import com.mirae.DailyBoost.food.domain.controller.model.response.FoodResponse;
 import com.mirae.DailyBoost.food.domain.business.FoodBusiness;
