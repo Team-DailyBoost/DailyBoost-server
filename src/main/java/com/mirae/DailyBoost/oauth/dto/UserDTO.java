aa02d53f9966449df1f225750a4a7fd9f827f063
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-11-06 18:43:21 +0900

    refactor: UserDTO에 담을 user 정보 수정

--- src/main/java/com/mirae/DailyBoost/oauth/dto/UserDTO.java
+++ src/main/java/com/mirae/DailyBoost/oauth/dto/UserDTO.java
@@ -6,24 +6,27 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Getter
+@Setter
 public class UserDTO {
   private Long id;
   private String email;
   private Role role;
-  private String provider;
-  private String providerId;
+  private String name;
+  private String nickname;
+
 
   public UserDTO(User user) {
     this.id = user.getId();
     this.email = user.getEmail();
     this.role = user.getRole();
-    this.provider = user.getProvider();
-    this.providerId = user.getProviderId();
+    this.name = user.getName();
+    this.nickname = user.getNickname();
   }
 
 }
