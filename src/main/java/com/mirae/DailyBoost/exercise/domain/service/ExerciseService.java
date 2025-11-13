af2d71bda1bc4bb4a5267c6f4af2ce8d70f90c56
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-11-13 23:55:36 +0900

    feat: 운동 추천과 기록 기능 구현

--- src/main/java/com/mirae/DailyBoost/exercise/domain/service/ExerciseService.java
+++ src/main/java/com/mirae/DailyBoost/exercise/domain/service/ExerciseService.java
@@ -3,6 +3,7 @@ package com.mirae.DailyBoost.exercise.domain.service;
 import com.mirae.DailyBoost.exercise.domain.repository.Exercise;
 import com.mirae.DailyBoost.exercise.domain.repository.ExerciseRepository;
 import com.mirae.DailyBoost.exercise.domain.repository.enums.ExerciseStatus;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,17 @@ public class ExerciseService {
 
   private final ExerciseRepository exerciseRepository;
 
-  public void register(Exercise exercise) {
-    exercise.initStatus();
+  public void save(Exercise exercise) {
     exerciseRepository.save(exercise);
+  }
+
+  public void register(Exercise exercise) {
+    exercise.register();
+    exerciseRepository.save(exercise);
+  }
+
+  public void saveAll(List<Exercise> exercises) {
+    exerciseRepository.saveAll(exercises);
   }
 
   public Optional<Exercise> getExercise(Long exerciseId, ExerciseStatus status) {
