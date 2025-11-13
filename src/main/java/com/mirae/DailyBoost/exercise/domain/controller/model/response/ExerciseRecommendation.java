d5b28e57e2fc1445b6f06bc617cbc27ff0d4dbf1
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-11-14 00:02:26 +0900

    refactor: 필드 변경

---
+++ src/main/java/com/mirae/DailyBoost/exercise/domain/controller/model/response/ExerciseRecommendation.java
@@ -0,0 +1,20 @@
+package com.mirae.DailyBoost.exercise.domain.controller.model.response;
+
+import com.mirae.DailyBoost.exercise.domain.repository.enums.ExercisePart;
+import com.mirae.DailyBoost.exercise.domain.repository.enums.Level;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExerciseRecommendation {
+  String name;
+  String description;
+  String youtubeLink;
+  Level level;
+  ExercisePart part;
+}
