b8655b5d5e9a55aa3a42c5563726cd0f152a8088
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-11-14 00:02:41 +0900

    feat: 운동 부위 추가

---
+++ src/main/java/com/mirae/DailyBoost/exercise/domain/repository/enums/ExercisePart.java
@@ -0,0 +1,12 @@
+package com.mirae.DailyBoost.exercise.domain.repository.enums;
+
+public enum ExercisePart {
+  CHEST,      // 가슴
+  BACK,       // 등
+  SHOULDER,   // 어깨
+  LOWER_BODY, // 하체
+  BICEPS,     // 이두
+  TRICEPS,    // 삼두
+  CARDIO,     // 유산소
+  HOME_TRAINING; // 홈트
+}
