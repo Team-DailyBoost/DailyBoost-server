b115969deb030c9c1a9d53d95c5e3b4f7b0de066
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-11-13 23:53:26 +0900

    feat: 상태 변경 메서드, 운동 부위 필드 추가

--- src/main/java/com/mirae/DailyBoost/exercise/domain/repository/Exercise.java
+++ src/main/java/com/mirae/DailyBoost/exercise/domain/repository/Exercise.java
@@ -1,15 +1,21 @@
 package com.mirae.DailyBoost.exercise.domain.repository;
 
 
+import com.mirae.DailyBoost.exercise.domain.repository.enums.CompletionStatus;
+import com.mirae.DailyBoost.exercise.domain.repository.enums.ExercisePart;
 import com.mirae.DailyBoost.exercise.domain.repository.enums.ExerciseStatus;
 import com.mirae.DailyBoost.exercise.domain.repository.enums.Level;
+import com.mirae.DailyBoost.user.domain.repository.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -40,13 +46,35 @@ public class Exercise {
   @Enumerated(value = EnumType.STRING)
   private ExerciseStatus status;
 
+  @Column(nullable = false)
+  @Enumerated(value = EnumType.STRING)
+  private CompletionStatus completionStatus;
+
   @Column(nullable = true, name = "youtube_link")
   private String youtubeLink;
 
-//  private User userId;
+  @Column(nullable = false)
+  @Enumerated(value = EnumType.STRING)
+  private ExercisePart part;
 
-  public void initStatus() {
-    this.status = ExerciseStatus.UNCOMPLETED;
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false)
+  private User user;
+
+  public void register() {
+    this.status = ExerciseStatus.REGISTERED;
+  }
+
+  public void unregister() {
+    this.status = ExerciseStatus.UNREGISTERED;
+  }
+
+  public void completed() {
+    this.completionStatus = CompletionStatus.COMPLETED;
+  }
+
+  public void uncompleted() {
+    this.completionStatus = CompletionStatus.UNCOMPLETED;
   }
 
 
