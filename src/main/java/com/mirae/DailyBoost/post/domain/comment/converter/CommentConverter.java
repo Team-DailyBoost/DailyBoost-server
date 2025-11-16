490cd7f328ddcfc9b299c5818f221567fd15b4a3
mirae <mirae7199@naver.com>
2025-11-16 20:24:53 +0900

    feat: imageUrl response로 응답 추가 구현

--- src/main/java/com/mirae/DailyBoost/post/domain/comment/converter/CommentConverter.java
+++ src/main/java/com/mirae/DailyBoost/post/domain/comment/converter/CommentConverter.java
@@ -1,6 +1,7 @@
 package com.mirae.DailyBoost.post.domain.comment.converter;
 
 import com.mirae.DailyBoost.global.annotation.Converter;
+import com.mirae.DailyBoost.image.domain.image.repository.Image;
 import com.mirae.DailyBoost.post.domain.comment.controller.model.request.CommentRequest;
 import com.mirae.DailyBoost.post.domain.comment.controller.model.response.CommentResponse;
 import com.mirae.DailyBoost.post.domain.comment.repository.Comment;
@@ -9,6 +10,7 @@ import com.mirae.DailyBoost.post.domain.post.repository.Post;
 import com.mirae.DailyBoost.user.domain.repository.User;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Converter
 public class CommentConverter {
@@ -32,6 +34,9 @@ public class CommentConverter {
           .createAt(comment.getCreateAt())
           .likeCount(comment.getLikeCount())
           .unLikeCount(comment.getUnLikeCount())
+          .imageUrl(Optional.ofNullable(comment.getImage())
+                  .map(Image::getUrl)
+                  .orElse(null))
           .build();
     }).toList();
 
